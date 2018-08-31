45484387eee1d5324fea3556f3117db23fb62293
Antonio Souto <antonio.souto@gruporbs.com.br>
2018-08-31 19:25:58 -0300

    Add scheduler and log

--- src/main/java/br/com/souto/Analytics.java
+++ src/main/java/br/com/souto/Analytics.java
@@ -2,6 +2,12 @@ package br.com.souto;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 import br.com.souto.analytics.SalesAnalysis;
 import br.com.souto.container.RegistersContainer;
@@ -11,26 +17,60 @@ import br.com.souto.repository.SalesData;
 import java.nio.file.Path;
 
 public class Analytics {
-	public static void main(String[] args) throws IOException {
-		
-       	SalesFiles.createDirs();
-        
-		for (Path path: SalesFiles.getInputFilePathList()) {
-			try {
-				System.out.println("Processing file " + path.toString());
-				List<String> registerLinesList = Repository.getRegisterLinesList(path);
-				List<RegistersContainer> registersContainersList =
-						new SalesData().getRegistersContainers(registerLinesList);
-				//System.out.println(registersContainersList);
-				String report = new SalesAnalysis().process(registersContainersList);
-				Repository.saveReport(report, SalesFiles.getOutputFilePath(path));
-			} catch (IndexOutOfBoundsException | NumberFormatException e) {
-				System.out.println("Invalid data in input file.");
-			} catch (IllegalArgumentException e) {
-				System.out.println(e.getMessage());
-			} catch (IOException e) {
-				System.out.println("File read/write error.");
-			}
+	
+	private final ScheduledExecutorService scheduler =
+		       Executors.newScheduledThreadPool(1);
+
+	final Logger logger = Logger.getLogger(this.getClass().getName());
+	
+	public void generateReportsJob() {
+
+		final Runnable reportsGenerator = new Runnable() {
+			public void run() {
+				logger.info("\nStarting Job...");
+				try {
+					for (Path path: SalesFiles.getInputFilePathList()) {
+						try {
+							
+							logger.info("Processing file " + path.toString());
+								
+							List<String> registerLinesList = Repository.getRegisterLinesList(path);
+							List<RegistersContainer> registersContainersList =
+									new SalesData().getRegistersContainers(registerLinesList);
+							
+							logger.fine(registersContainersList.toString());
+							
+							String report = new SalesAnalysis().process(registersContainersList);
+							
+							Repository.saveReport(report, SalesFiles.getOutputFilePath(path));
+								
+						} catch (IndexOutOfBoundsException | NumberFormatException e) {
+								logger.log(Level.SEVERE, "Invalid data in input file.");
+						} catch (IllegalArgumentException e) {
+								System.out.println(e.getMessage());
+						} catch (IOException e) {
+							logger.log(Level.SEVERE, "File read/write error: " + e.getMessage());
+						}
+					}
+				} catch (IOException e) {
+					logger.log(Level.SEVERE, "File read/write error: " + e.getMessage());
+				}
+			}};
+			final ScheduledFuture<?> reportsGeneratorHandler =
+		            scheduler.scheduleAtFixedRate(reportsGenerator, 10, 10, TimeUnit.SECONDS);
+			
 		}
+	
+	public static void main(String[] args) {
+        
+		try {
+			SalesFiles.createDirs();
+			Analytics analytics = new Analytics();
+			analytics.generateReportsJob();
+			
+		} catch (IOException e) {
+			System.out.println("ERROR: File read/write error: " + e.getMessage());
+		}
+
     }
 }
--- src/main/java/br/com/souto/repository/SalesData.java
+++ src/main/java/br/com/souto/repository/SalesData.java
@@ -1,16 +1,9 @@
 package br.com.souto.repository;
 
-import java.io.IOException;
-
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import br.com.souto.container.ClientRegistersContainer;
 import br.com.souto.container.RegistersContainer;
